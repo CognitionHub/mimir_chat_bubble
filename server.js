2590d560102659daaea1766772e6a683d48ae265
Jens <jensakrr@gmail.com>
2023-11-02 15:34:09 +0100

    QoL improvements

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const livereload = require('livereload');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 const liveReloadServer = livereload.createServer();
 liveReloadServer.watch(path.join(__dirname, 'public'));
@@ -12,7 +12,7 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
     import('open').then(open => {
-        open.default(`http://localhost:${port}/blank.html`);
+        open.default(`http://localhost:${port}`);
     });
 })
 
